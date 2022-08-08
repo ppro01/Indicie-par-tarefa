52eb2e9fed8a5fb3ac801a365ce5dbea127849a7
Francisco P <101436738+ppro01@users.noreply.github.com>
2022-08-07 23:54:03 -0300

    adcionando o programa
    Este programa pede 15 números, armazena em um vetor e depois mostra apenas os números que estão posicionados nos índices de valor positivo.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        printEven(getVector());
+    }
+
+    static void printEven(int[] allPositions)
+    {
+        for (int i = 0; i  < allPositions.Length; i++){
+            if (i % 2 == 0)
+            {
+                Console.WriteLine("o {0}° numero é {1}", i + 1, allPositions[i]);
+            }
+        }
+    }
+
+    static int[] getVector()
+    {
+        int[] givenVector = new int[15];
+
+        for (int i = 0; i < givenVector.Length; i++)
+        {
+            Console.WriteLine("Digite o {0}° número", i+1 );
+            givenVector[i] = int.Parse(Console.ReadLine());
+
+        }
+
+        return givenVector;
+    }
+
+
+}
